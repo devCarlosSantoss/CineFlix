226125700b6269eb49f8d3d7c8bc317f789695ce
Ivanildo Campos <125228040+IvanildoCampos@users.noreply.github.com>
2023-07-27 23:10:07 -0300

    rotas api
    começnado gerar as rotas de apis

--- public/js/index.js
+++ public/js/index.js
@@ -19,3 +19,28 @@ async function getNowPlaying(){
 };
 
 getNowPlaying();
+
+const express = require('express');
+const axios = require('axios');
+const app = express();
+const port = 3000;
+
+// Rota para obter os filmes em exibição (now playing) da API do TMDb
+app.get('/filmes-em-exibicao', async (req, res) => {
+  try {
+    const apiURL = 'https://api.themoviedb.org/3/movie/now_playing?language=pt-BR&page=1&sort_by=popularity.desc';
+    const apiKey = 'sua_chave_de_api_do_tmdb'; // Substitua pela sua chave de API do TMDb
+
+    const response = await axios.get(`${apiURL}&api_key=${apiKey}`);
+    const filmes = response.data.results;
+    res.json(filmes);
+  } catch (error) {
+    console.error('Erro ao obter filmes em exibição:', error.message);
+    res.status(500).json({ mensagem: 'Erro ao obter filmes em exibição' });
+  }
+});
+
+// Iniciar o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
